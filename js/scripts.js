ed583f6475cf59c477a4d457943ad3fe79ca4570
Grady L Shelton <gradyish@gmail.com>
2017-08-08 16:18:37 -0700

    add scroll effect for projects from nav link

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
 
+
+
 $(document).ready(function(){
+  //initial fade in of mast head and header elements
   $(window).ready(function() {
     $('.masthead').fadeIn(2000);
     $('#masthead-subtext').fadeIn(5000);
     $("#developer").fadeIn(6000);
     $('.jumbotron').fadeIn(4000);
   });
+
+  //scroll to projects from nav
+  $("#project-scroll-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#projects-section").offset().top
+    }, 2000);
+  });
+
+  //scroll to technologies from nav
+
+  //scroll to background from nav
+
+  //scroll to contact from nav
+
+
+
 });
